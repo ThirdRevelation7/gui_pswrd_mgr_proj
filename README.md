402139ee3dacae3889c80ad72c05901f3b91b56c
Christian <150170670+ThirdRevelation7@users.noreply.github.com>
2024-02-06 09:05:20 -0700

    Added several features Added Search feature and changed file type from .txt to .json

--- README.md
+++ README.md
@@ -5,10 +5,14 @@ A simple Password Manager and Password Generator to store your passwords locally
 * Save website, email/usrname, and password to a data.txt file on your computer.
 * Generate a strong randomized password
 * Automatically copies password to your clipboard when generating, for easy paste.
-* Coming Soon - A search feature to look up previously saved entries
+
 
 # Motivation
 I wanted to make a password manager that built on an old program I made when I first started coding. The old program was simply a randomized password generator
 that ran in the terminal. At the time it was a very daunting and difficult program to make. Part of that code still exists in my new program, but it's only one tiny part 
 of a much larger and improved program. I'm still in the early stages of my Programming Journey, and I still have sooo much to learn and improve. However, this was a 
 giant milestone for me, and it's extremely motivating to see how much I've learned and improved in just a couple of months. 
+
+# Updates
+* Now saves information to a data.json file instead of .txt.
+* Search feature, that allows you to search saved entries and auto copys password to clipboard.
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
 import pyperclip
+import json
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-# Password Generator Project
 
 
 def password_generator():
@@ -24,27 +25,59 @@ def password_generator():
     pyperclip.copy(final_password)
 
 
+# # ----------------------------SEARCH FEATURE-------------------------------- #
+
+
+def search():
+    with open("data.json", "r") as data_file:
+        user_accounts = json.load(data_file)
+        website = website_entry.get().capitalize()
+        try:
+            users_search = user_accounts[website]
+        except KeyError:
+            messagebox.showerror(title="Not Found", message=f"Sorry, no entry for '{website}' found 😢")
+        else:
+            messagebox.showinfo(title="Here", message=f"Username: {users_search["email"]} \nPassword: {
+                                users_search["password"]} \nYour password is ready to paste! ")
+            pyperclip.copy(users_search["password"])
+
+
 # # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
 def save_entry():
-    website = website_entry.get()
+    website = website_entry.get().capitalize()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
 
-    ok_to_save = messagebox.askokcancel(title="Details Entered", message=f"Your Entries \nWebsite: {
-                                        website} \nEmail: {email} \nPassword: {password} \nIs it ok to save?")
+        }
+    }
 
-    if ok_to_save:
-        if len(website) > 0 and len(password) > 0:
-            with open("data.txt", "a") as file:
-                file.write(f"Website: {website} | Email: {email} | Password: {password} \n ")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
-                email_entry.delete(0, END)
-                email_entry.insert(0, "youremail@email.com")
+    if len(website) > 0 and len(password) > 0:
+        try:
+            with open("data.json", "r") as data_file:
+                # read/load data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
         else:
-            messagebox.showerror(title="Ooops", message="Oopsies...Looks like you didn't fill in all required info 😳")
+            # Update data with new data
+            data.update(new_data)
+            # Write new data to json file
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
+            messagebox.showinfo(title="Success", message="Your information was saved successfully!")
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+            email_entry.delete(0, END)
+    else:
+        messagebox.showerror(title="Ooops", message="Oopsies...Looks like you didn't fill in all required info 😳")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -59,9 +92,9 @@ canvas.create_image(100, 100, image=lock_image)
 canvas.grid(column=1, row=0)
 
 # Entry/label for web
-website_entry = Entry(width=34)
+website_entry = Entry(width=21)
 website_entry.focus()
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.grid(column=1, row=1)
 web_lbl = Label(text="Website:", fg="white", bg="black", font=("Arial", 14))
 web_lbl.grid(column=0, row=1)
 
@@ -79,13 +112,19 @@ password_entry.grid(column=1, row=3)
 password_entry_lbl = Label(text="Password:", fg="white", bg="black", font=("Arial", 14))
 password_entry_lbl.grid(column=0, row=3)
 
-generate_password_button = Button(text="Generate Password", width=12, activeforeground="blue",
-                                  highlightbackground="black", fg="black", font=("Arial", 10), command=password_generator)
+generate_password_button = Button(text="Generate Password", width=12, activeforeground="blue", fg="black",
+                                  highlightbackground="black", font=("Arial", 10), command=password_generator)
 generate_password_button.grid(column=2, row=3)
 
 # Add button
-add_password = Button(text="Add", width=32, activeforeground="blue", command=save_entry)
+add_password = Button(text="Add", width=32, highlightbackground="black", activeforeground="blue", command=save_entry)
 add_password.grid(column=1, row=4, columnspan=2)
+
+# Search button
+search_button = Button(text="Search", width=8, highlightbackground="black", activeforeground="blue",
+                       command=search)
+search_button.grid(column=2, row=1)
+
 
 
 window.mainloop()
